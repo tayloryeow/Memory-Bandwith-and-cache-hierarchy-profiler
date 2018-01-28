3866d6d4e49877e39a2b1d4af8ba6cfa31c37f3e
Lrd Taylor FMY Stinson <tayloryeow@gmail.com>
2018-01-28 15:38:39 -0500

    implemented rudmentary nanosecond graphs for testbench

--- A1/part1/graph.py
+++ A1/part1/graph.py
@@ -2,13 +2,22 @@ import random
 
 import leather
 
-dot_data = [(random.randint(0, 250), random.randint(0, 250)) for i in range(100)]
+#Parse benchmark data into dat_data
 
-def colorizer(d):
-    return 'rgd(%i, %i, %i)' % (d.x, d.y, 150)
+test="benchmark"
 
-chart = leather.Chart('Colorized dots')
-chart.add_dots(dot_data, fill_color=colorizer)
-chart.to_svg('examples/charts/colorized_dots.svg')
+i = 0;
+f = open('data/'+test, 'r')
+dot_data = []
+for line in f:
+    dot_data.append((int(i), (map(int, line.split(' '))[1])))
+    print dot_data[-1]
+    i+= 1
+print dot_data
+f.close()
+
+chart = leather.Chart('Charts: '+test)
+chart.add_line(dot_data)
+chart.to_svg('examples/charts/'+ test +'.svg')
 
 
