94a0950a7e07276c8c0194f203e1869b416b81a3
Lrd Taylor FMY Stinson <tayloryeow@gmail.com>
2018-01-28 16:14:18 -0500

    streamlined graphing so it automatically pop's up the graph

--- A1/part1/graph.py
+++ A1/part1/graph.py
@@ -1,13 +1,18 @@
 import random
+import argparse
 
 import leather
 
 #Parse benchmark data into dat_data
 
-test="benchmark"
+parser = argparse.ArgumentParser(description='Plot testing data')
+parser.add_argument('testName', type=str, help='Name of the Test File')
 
+args = parser.parse_args()
+
+print args.testName
 i = 0;
-f = open('data/'+test, 'r')
+f = open('data/'+ args.testName, 'r')
 dot_data = []
 for line in f:
     dot_data.append((int(i), (map(int, line.split(' '))[1])))
@@ -16,8 +21,8 @@ for line in f:
 print dot_data
 f.close()
 
-chart = leather.Chart('Charts: '+test)
+chart = leather.Chart('Charts: '+ args.testName)
 chart.add_line(dot_data)
-chart.to_svg('examples/charts/'+ test +'.svg')
-
+chart.to_svg('examples/charts/'+ args.testName +'.svg')
+print "Printed file to " + "examples/charts" + args.testName + ".svg"
 
