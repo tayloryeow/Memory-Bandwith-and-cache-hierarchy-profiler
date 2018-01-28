99657f64ee72a258f86c288fcdacf8288ef05e35
Lrd Taylor FMY Stinson <tayloryeow@gmail.com>
2018-01-28 13:01:29 -0500

    added simple graphing tool using python2 leather

---
+++ A1/part1/graph.py
@@ -0,0 +1,14 @@
+import random
+
+import leather
+
+dot_data = [(random.randint(0, 250), random.randint(0, 250)) for i in range(100)]
+
+def colorizer(d):
+    return 'rgd(%i, %i, %i)' % (d.x, d.y, 150)
+
+chart = leather.Chart('Colorized dots')
+chart.add_dots(dot_data, fill_color=colorizer)
+chart.to_svg('examples/charts/colorized_dots.svg')
+
+
